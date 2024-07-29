93286c17217ca2350c913d044b922dad58032dae
Nandeesha <nandes@gmail.com>
2024-07-29 21:44:56 +0530

    app js file edited

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React Hellow Nandeesha whare are you
         </a>
       </header>
     </div>
